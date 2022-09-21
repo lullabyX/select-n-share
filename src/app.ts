620356df7182b74d58ee5310ec74a5b50a0c52ad
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-21 19:55:14 +0600

    added google map

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,47 @@
-console.log('====================================');
-console.log('Starting Setup');
-console.log('====================================');
+import axios from "axios";
+
+const form = document.querySelector("form") as HTMLFormElement;
+
+const GOOGLE_MAP_API_KEY = "AIzaSyBPxD365rZocu5KOxcNAL9n7mcQMO3YwJQ";
+
+const addressSubmitHandler = (event: Event) => {
+  event.preventDefault();
+  const inputEl = document.getElementById("address") as HTMLInputElement;
+  const address = inputEl.value;
+  if (address.trim().length === 0) {
+    return;
+  }
+
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        address
+      )}&key=${GOOGLE_MAP_API_KEY}`
+    )
+    .then((response) => {
+      
+      if (response.data.status !== "OK") {
+        throw new Error("Cannot resolve address. Please try again later.");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(
+        document.getElementById("map") as HTMLElement,
+        {
+          center: coordinates,
+          zoom: 16,
+        }
+      );
+      new google.maps.Marker({
+        position: coordinates,
+        map: map,
+      });
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log("====================================");
+      console.log(err);
+      console.log("====================================");
+    });
+};
+
+form.addEventListener("submit", addressSubmitHandler);
